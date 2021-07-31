35f9d95c43c122b04bbdb1dc8ebcab8ad44e7e54
Cedric <limacedric@hotmail.fr>
2021-08-01 00:32:20 +0200

    test

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                       About
+                       Aboute
                     </div>
                 </div>
             </div>
